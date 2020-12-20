9e69cb4af8e9ebddfeed3df4f3947c4f5ccfbbca
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2020-12-20 19:28:51 +0000

    ssd2

--- ssd2.py
+++ ssd2.py
@@ -13,11 +13,44 @@ response = smartsheet_client.Sheets.list_sheets(include="attachments,source", in
 sheets = response.data
 time.sleep(2)
 print(type(sheets))
+
 time.sleep(2)
+print(sheets)
 print("OK")
 
-ss = sheets.to_json()
-print(type(ss))
-time.sleep(2)
-print(ss)
+for x in range(5):
+  print(sheets[x].name)
+  print(sheets[x].id)
+  
+  sname = sheets[x].name
+  if(sname == "Ajay Malik"):
+    print("\t hello")
+    id = sheets[x].id
+    sht = sheetssheets[x]
+    rs = sht.total_row_count
+    time.sleep(2)
+    print(type(rs))
+    print(rs)
+    r = rs.rows
+    print(type(r))
+    print(r)
+    for s in rs:
+      print(s)
+    time.sleep(2)
+    #print_data(sht,rid)
+  time.sleep(3)
+
+def print_data(sheet, row_id):
+  row_data = sheet.get_row(row_id)
+  print(type(row_data))
+"""
+for s in sheets:
+  print(type(s))
+  print(s)
+  time.sleep(2)
+  print(type(s.name))
+  print(s.name)
+  time.sleep(2)
+"""
+
 print("now")

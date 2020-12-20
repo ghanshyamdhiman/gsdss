f4508725fb34dcf8eeccb5e60a90f419bc3325ff
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2020-12-20 21:08:14 +0530

    Update ss1.py

--- ss1.py
+++ ss1.py
@@ -6,6 +6,12 @@ smartsheet_client = smartsheet.Smartsheet(access_token)
 
 # Make sure we don't miss any errors
 smartsheet_client.errors_as_exceptions(True)
+
+"""
+response = smartsheet_client.Sheets.list_sheets(include="attachments,source", include_all=True)
+sheets = response.data
+"""
+
 response = smartsheet_client.Favorites.list_favorites(include_all=True)
 faves = response.data
 print(type(faves))

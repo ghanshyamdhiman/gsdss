6d6ae1b260f363c43da0786e3f8dd7334db70910
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2020-12-23 22:47:58 +0530

    Create wm2.py

---
+++ wm2.py
@@ -0,0 +1,6 @@
+ curl -X POST https://api.gupshup.io/sm/api/v1/msg \
+ -H 'Cache-Control: no-cache' \
+ -H 'Content-Type: application/x-www-form-urlencoded' \
+ -H 'apikey: 8jto5uzk3utwsrlhjkokftlrrkjtkkfb' \
+ -H 'cache-control: no-cache' \
+ -d 'channel=whatsapp&source=917834811114&destination=919503457231&message=%7B%22type%22:%22text%22,%22text%22:%22API%20TEST%20MSG%22%7D&src.name=GSDJDDLJ' 
